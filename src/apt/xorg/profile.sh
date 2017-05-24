dda4cc582850a40d86919064a35f18c8cf7d2fea
Topher Bullock <cbullock@pivotal.io>
2017-05-23 21:32:13 -0400

    Add package, job for xorg

---
+++ src/apt/xorg/profile.sh
@@ -0,0 +1,5 @@
+export PATH="/var/vcap/packages/xorg/apt/usr/bin:$PATH"
+export LD_LIBRARY_PATH="/var/vcap/packages/xorg/apt/usr/lib:${LD_LIBRARY_PATH:-}"
+export INCLUDE_PATH="/var/vcap/packages/xorg/apt/usr/include:${INCLUDE_PATH:-}"
+export CPATH="$INCLUDE_PATH"
+export CPPPATH="$INCLUDE_PATH"

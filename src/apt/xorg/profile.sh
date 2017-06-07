08e9f47e85256ada0acfcda1eca61b548c0eb9dd
Topher Bullock <cbullock@pivotal.io>
2017-06-06 22:28:18 -0400

    switch up the game on dependancy packaging

--- src/apt/xorg/profile.sh
+++
@@ -1,5 +0,0 @@
-export PATH="/var/vcap/packages/xorg/apt/usr/bin:$PATH"
-export LD_LIBRARY_PATH="/var/vcap/packages/xorg/apt/usr/lib:${LD_LIBRARY_PATH:-}"
-export INCLUDE_PATH="/var/vcap/packages/xorg/apt/usr/include:${INCLUDE_PATH:-}"
-export CPATH="$INCLUDE_PATH"
-export CPPPATH="$INCLUDE_PATH"
---
+++ utils/deb-fetcher.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cahe_dir=$1
+apt-get install -d -o dir::cache::archives="${cache_dir}" <package>
